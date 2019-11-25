6164d793c366cd388087d7706e3e08d727cccbda
czh <czhwin@outlook.com>
2019-11-25 13:35:52 +0800

    nodejs

--- python/test.py
+++ python/test.py
@@ -2,7 +2,13 @@ import platform
 from logging import log
 from HttpClient import WindowsChrome
 from bs4 import BeautifulSoup
-from downloader import download_maven, __get_maven_version
+import requests
 
 if __name__ == "__main__":
-    __get_maven_version()
+    # https://nodejs.org/en/
+    # https://nodejs.org/dist/v12.13.1/node-v12.13.1-x64.msi
+    # https://nodejs.org/dist/v12.13.1/node-v12.13.1-linux-x64.tar.xz
+    node_html = requests.get('https://nodejs.org/en/').text
+    soup = BeautifulSoup(node_html, 'lxml')
+    download_button = soup.select('.home-downloadbutton')[0]
+    print(download_button.attrs['data-version'])
